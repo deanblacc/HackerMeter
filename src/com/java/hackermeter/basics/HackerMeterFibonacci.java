1cdf63070f761791e2cf5b656eef724db1313d29
deanblacc <deanblacc@googlemail.com>
2013-09-01 19:21:04 +0100

    Solution for HackerMeter Basics: Palindrome Perception question

--- src/com/java/hackermeter/basics/HackerMeterFibonacci.java
+++ src/com/java/hackermeter/basics/HackerMeterFibonacci.java
@@ -3,10 +3,11 @@ package com.java.hackermeter.basics;
 import java.util.Scanner;
 
 /**
- * Solution for hackerMeter basics-fibonacci.
+ * Solution for HackerMeter Basics:Fibonacci.
  */
 public class HackerMeterFibonacci {
     public static void run(Scanner scanner) {
+        //Code here!
         int fibNum = Integer.parseInt(scanner.nextLine());
 
         long x = 0, y = 1, i = 0;
---
+++ src/com/java/hackermeter/basics/HackerMeterPalindromePerception.java
@@ -0,0 +1,30 @@
+package com.java.hackermeter.basics;
+
+import java.util.Scanner;
+
+/**
+ * Solution for HackerMeter Basics: PalindromePerception.
+ */
+public class HackerMeterPalindromePerception {
+    public static void run(Scanner scanner) {
+        //Code here!
+        String palindrome = scanner.nextLine();
+
+        for(int idx = 0; idx < (palindrome.length()/2)+1; idx++) {
+            int left = idx, right = palindrome.length() - idx - 1;
+            if(palindrome.charAt(left) != palindrome.charAt(right)) {
+                System.out.println("N");
+                break;
+            } else if(left == right || left + 1 == right)
+                System.out.println("Y");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = Integer.parseInt(scanner.nextLine());
+        for(int i = 0; i < cases; i++) {
+            run(scanner);
+        }
+    }
+}
